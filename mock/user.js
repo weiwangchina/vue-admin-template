2a52e67f62340ff0af2874ae46902c13bf90cf12
wangwei <515002954@qq.com>
2019-07-31 10:50:59 +0800

    编辑菜单为中文

--- mock/user.js
+++ mock/user.js
@@ -13,7 +13,7 @@ const users = {
     roles: ['admin'],
     introduction: 'I am a super administrator',
     avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
-    name: 'Super Admin'
+    name: 'Super Admin.'
   },
   'editor-token': {
     roles: ['editor'],
@@ -28,7 +28,7 @@ export default [
   {
     url: '/user/login',
     type: 'post',
-    response: config => {
+    response: (config) => {
       const { username } = config.body
       const token = tokens[username]
 
